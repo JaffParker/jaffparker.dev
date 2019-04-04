423a63270660439c39ac9cf97819601b1b16c96e
Andrey Kamozin <andreykamozin@gmail.com>
2019-04-03 21:30:36 -0400

    Added a pic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,18 @@
+const { join } = require('path')
+
 module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: join(__dirname, 'src', 'images'),
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
   ],
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,18 +4,42 @@ import styled from 'styled-components'
 import tw from 'tailwind.macro'
 import Helmet from 'react-helmet'
 import { Logo } from '../components/Logo'
+import { graphql } from 'gatsby'
+import GatsbyImage from 'gatsby-image'
 
 const Container = styled.div`
-  ${tw`h-screen w-screen flex justify-center items-center`}
+  ${tw`h-screen w-screen flex justify-center items-center flex-col`}
+`
+const Image = styled(GatsbyImage)`
+  ${tw`w-64 h-64 m-4 rounded-full`}
 `
 
-export default () => (
+export default ({
+  data: {
+    file: {
+      childImageSharp: { fluid: logoFluid },
+    },
+  },
+}) => (
   <Fragment>
     <Helmet>
       <title>Jaff Parker</title>
     </Helmet>
     <Container>
+      <Image fluid={logoFluid} />
       <Logo />
     </Container>
   </Fragment>
 )
+
+export const query = graphql`
+  {
+    file(relativePath: { eq: "AndreyBogdanov2.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 300, maxHeight: 300) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
